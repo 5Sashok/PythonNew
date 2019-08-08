208aa176e0be2d2e4387d075a30a0f6ba7e1ff98
5Sashok <neox@meta.ua>
2019-08-08 23:15:33 +0300

    Delete dz1_2_country.py

--- dz1_2_country.py
+++
@@ -1,5 +0,0 @@
-dict_coun = {"France":"Paris", "Italy":"Rom", "Poland":"Warsaw", "Cuba":"Gavana", "Ukraine":"Kyiv"}
-list_count = ["Ukraine", "USA", "Italy", "Argentina", "Chili"]
-
-for i in list_count:
- if i in dict_coun: print(dict_coun[i])
